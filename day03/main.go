7fc6e4f7d58e7c917ef6fc4179f0b1959a7098e8
Lux Barker <29106316+k-nox@users.noreply.github.com>
2023-12-04 15:16:19 -0800

    set up day 3

---
+++ day03/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/k-nox/aoc2023/day03/partone"
+	"github.com/k-nox/aoc2023/day03/parttwo"
+)
+
+func main() {
+	fmt.Printf("Solution for part one: %d\n", partone.Solve())
+	fmt.Printf("Solution for part two: %d\n", parttwo.Solve())
+}
---
+++ day03/parttwo/parttwo.go
@@ -0,0 +1,5 @@
+package parttwo
+
+func Solve() int {
+	return 0
+}
