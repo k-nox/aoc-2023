364161cd42c5a85338df1637f87af70edb7ede4e
Lux Barker <29106316+k-nox@users.noreply.github.com>
2023-12-06 08:45:44 -0800

    solve part 2

--- day06/parttwo/parttwo.go
+++ day06/parttwo/parttwo.go
@@ -1,5 +1,37 @@
 package parttwo
 
+import (
+	"github.com/k-nox/aoc2023/day06/partone"
+	"github.com/k-nox/aoc2023/util"
+	"strconv"
+	"strings"
+)
+
 func Solve() int {
-	return 0
+	f := util.NewScannerForInput(6, false)
+	defer f.Close()
+
+	race := parseInput(f)
+
+	return race.FindNumberOfPossibleWins()
+}
+
+func parseInput(f *util.FileScanner) partone.Race {
+	race := partone.Race{}
+
+	for f.Scan() {
+		line := f.Text()
+		numStr := strings.ReplaceAll(strings.Split(line, ":")[1], " ", "")
+		num, err := strconv.Atoi(numStr)
+		if err != nil {
+			panic(err)
+		}
+		if race.Time == 0 {
+			// we are parsing the time
+			race.Time = num
+		} else {
+			race.Distance = num
+		}
+	}
+	return race
 }
